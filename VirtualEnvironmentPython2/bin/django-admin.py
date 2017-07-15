6aa595811b8de86e2dcbe18f0c7932c3b91cb447
Lynn Samson <lasamson@umass.edu>
2017-07-15 12:57:17 -0700

    first commit

---
+++ VirtualEnvironmentPython2/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/lynnsamson/Desktop/ViaSat_Hackathon/VirtualEnvironmentPython2/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
