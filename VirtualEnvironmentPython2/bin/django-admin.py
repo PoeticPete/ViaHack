087dae15152a9089de82d4e7a046305bb485eb00
PoeticPete <petertao95@gmail.com>
2017-10-30 03:03:03 -0400

    unlocks door now

--- VirtualEnvironmentPython2/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/lynnsamson/Desktop/ViaSat_Hackathon/VirtualEnvironmentPython2/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
